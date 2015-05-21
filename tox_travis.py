e572cd791e03efbf9715b6597b7bd427e23ba16e
Ryan Hiebert <ryan@ryanhiebert.com>
2015-05-21 02:00:57 -0500

    Set some reasonable defaults

--- tox_travis.py
+++ tox_travis.py
@@ -4,6 +4,18 @@ import py
 import tox
 
 
+TOX_DEFAULTS = {
+    '2.6': 'py26',
+    '2.7': 'py27',
+    '3.2': 'py32',
+    '3.3': 'py33',
+    '3.4': 'py34',
+    '3.5': 'py35',
+    'pypy': 'pypy',
+    'pypy3': 'pypy3',
+}
+
+
 @tox.hookimpl
 def tox_addoption(parser):
     if 'TRAVIS' not in os.environ:
@@ -13,7 +25,7 @@ def tox_addoption(parser):
 
     config = py.iniconfig.IniConfig('tox.ini')
     section = config.sections.get('tox:travis', {})
-    envlist = section.get(version)
+    envlist = section.get(version, TOX_DEFAULTS.get(version))
 
     if envlist:
         os.environ.setdefault('TOXENV', envlist)
