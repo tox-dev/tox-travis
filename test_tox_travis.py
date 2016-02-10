80d725cad672e85cc84d3bdfcf04292e199b8257
Ryan Hiebert <ryan@ryanhiebert.com>
2016-02-10 12:40:14 -0600

    Refactor tests

--- test_tox_travis.py
+++ test_tox_travis.py
@@ -53,165 +53,97 @@ class TestToxTravis:
             ['tox', '-l'], stdout=subprocess.PIPE).communicate()[0]
         return output.decode('utf-8').strip().split()
 
-    def test_not_travis(self, tmpdir):
+    def configure(self, tmpdir, monkeypatch, tox_ini,
+                  version=None, major=None, minor=None):
         os.chdir(str(tmpdir))
         tmpdir.join('tox.ini').write(tox_ini)
 
+        if version:
+            sys_version = '{version},{major},{minor}'.format(
+                version=version, major=major, minor=minor)
+            travis_version = '{major}.{minor}'.format(
+                major=major, minor=minor)
+            if version == 'PyPy':
+                travis_version = 'pypy3' if major == 3 else 'pypy'
+
+            monkeypatch.setenv('TRAVIS', 'true')
+            monkeypatch.setenv('TRAVIS_PYTHON_VERSION', travis_version)
+            monkeypatch.setenv('__TOX_TRAVIS_SYS_VERSION', sys_version)
+
+
+    def test_not_travis(self, tmpdir, monkeypatch):
+        self.configure(tmpdir, monkeypatch, tox_ini)
         expected = [
             'py26', 'py27', 'py32', 'py33', 'py34',
             'pypy', 'pypy3', 'docs',
         ]
-
         assert self.tox_envs() == expected
 
     def test_travis_default_26(self, tmpdir, monkeypatch):
-        os.chdir(str(tmpdir))
-        tmpdir.join('tox.ini').write(tox_ini)
-
-        monkeypatch.setenv('TRAVIS', 'true')
-        monkeypatch.setenv('TRAVIS_PYTHON_VERSION', '2.6')
-        monkeypatch.setenv('__TOX_TRAVIS_SYS_VERSION', 'CPython,2,6')
-
+        self.configure(tmpdir, monkeypatch, tox_ini, 'CPython', 2, 6)
         assert self.tox_envs() == ['py26']
 
     def test_travis_default_27(self, tmpdir, monkeypatch):
-        os.chdir(str(tmpdir))
-        tmpdir.join('tox.ini').write(tox_ini)
-
-        monkeypatch.setenv('TRAVIS', 'true')
-        monkeypatch.setenv('TRAVIS_PYTHON_VERSION', '2.7')
-        monkeypatch.setenv('__TOX_TRAVIS_SYS_VERSION', 'CPython,2,7')
-
+        self.configure(tmpdir, monkeypatch, tox_ini, 'CPython', 2, 7)
         assert self.tox_envs() == ['py27']
 
     def test_travis_default_32(self, tmpdir, monkeypatch):
-        os.chdir(str(tmpdir))
-        tmpdir.join('tox.ini').write(tox_ini)
-
-        monkeypatch.setenv('TRAVIS', 'true')
-        monkeypatch.setenv('TRAVIS_PYTHON_VERSION', '3.2')
-        monkeypatch.setenv('__TOX_TRAVIS_SYS_VERSION', 'CPython,3,2')
-
+        self.configure(tmpdir, monkeypatch, tox_ini, 'CPython', 3, 2)
         assert self.tox_envs() == ['py32']
 
     def test_travis_default_33(self, tmpdir, monkeypatch):
-        os.chdir(str(tmpdir))
-        tmpdir.join('tox.ini').write(tox_ini)
-
-        monkeypatch.setenv('TRAVIS', 'true')
-        monkeypatch.setenv('TRAVIS_PYTHON_VERSION', '3.3')
-        monkeypatch.setenv('__TOX_TRAVIS_SYS_VERSION', 'CPython,3,3')
-
+        self.configure(tmpdir, monkeypatch, tox_ini, 'CPython', 3, 3)
         assert self.tox_envs() == ['py33']
 
     def test_travis_default_34(self, tmpdir, monkeypatch):
-        os.chdir(str(tmpdir))
-        tmpdir.join('tox.ini').write(tox_ini)
-
-        monkeypatch.setenv('TRAVIS', 'true')
-        monkeypatch.setenv('TRAVIS_PYTHON_VERSION', '3.4')
-        monkeypatch.setenv('__TOX_TRAVIS_SYS_VERSION', 'CPython,3,4')
-
+        self.configure(tmpdir, monkeypatch, tox_ini, 'CPython', 3, 4)
         assert self.tox_envs() == ['py34']
 
     def test_travis_default_pypy(self, tmpdir, monkeypatch):
-        os.chdir(str(tmpdir))
-        tmpdir.join('tox.ini').write(tox_ini)
-
-        monkeypatch.setenv('TRAVIS', 'true')
-        monkeypatch.setenv('TRAVIS_PYTHON_VERSION', 'pypy')
-        monkeypatch.setenv('__TOX_TRAVIS_SYS_VERSION', 'PyPy,2,7')
-
+        self.configure(tmpdir, monkeypatch, tox_ini, 'PyPy', 2, 7)
         assert self.tox_envs() == ['pypy']
 
     def test_travis_default_pypy3(self, tmpdir, monkeypatch):
-        os.chdir(str(tmpdir))
-        tmpdir.join('tox.ini').write(tox_ini)
-
-        monkeypatch.setenv('TRAVIS', 'true')
-        monkeypatch.setenv('TRAVIS_PYTHON_VERSION', 'pypy3')
-        monkeypatch.setenv('__TOX_TRAVIS_SYS_VERSION', 'PyPy,3,2')
-
+        self.configure(tmpdir, monkeypatch, tox_ini, 'PyPy', 3, 2)
         assert self.tox_envs() == ['pypy3']
 
     def test_travis_override(self, tmpdir, monkeypatch):
-        os.chdir(str(tmpdir))
-        tmpdir.join('tox.ini').write(tox_ini_override)
-
-        monkeypatch.setenv('TRAVIS', 'true')
-        monkeypatch.setenv('TRAVIS_PYTHON_VERSION', '2.7')
-        monkeypatch.setenv('__TOX_TRAVIS_SYS_VERSION', 'CPython,2,7')
-
+        tox_ini = tox_ini_override
+        self.configure(tmpdir, monkeypatch, tox_ini, 'CPython', 2, 7)
         assert self.tox_envs() == ['py27', 'docs']
 
     def test_respect_overridden_toxenv(self, tmpdir, monkeypatch):
-        os.chdir(str(tmpdir))
-        tmpdir.join('tox.ini').write(tox_ini)
-
-        monkeypatch.setenv('TRAVIS', 'true')
-        monkeypatch.setenv('TRAVIS_PYTHON_VERSION', '2.7')
-        monkeypatch.setenv('__TOX_TRAVIS_SYS_VERSION', 'CPython,2,7')
+        self.configure(tmpdir, monkeypatch, tox_ini, 'CPython', 2, 7)
         monkeypatch.setenv('TOXENV', 'py32')
-
         assert self.tox_envs() == ['py32']
 
     def test_keep_if_no_match(self, tmpdir, monkeypatch):
-        os.chdir(str(tmpdir))
-        tmpdir.join('tox.ini').write(tox_ini_factors)
-
-        monkeypatch.setenv('TRAVIS', 'true')
-        monkeypatch.setenv('TRAVIS_PYTHON_VERSION', '2.7')
-        monkeypatch.setenv('__TOX_TRAVIS_SYS_VERSION', 'CPython,2,7')
-
+        tox_ini = tox_ini_factors
+        self.configure(tmpdir, monkeypatch, tox_ini, 'CPython', 2, 7)
         assert self.tox_envs() == ['py27']
 
     def test_default_tox_ini_overrides(self, tmpdir, monkeypatch):
-        os.chdir(str(tmpdir))
-        tmpdir.join('tox.ini').write(tox_ini_factors_override)
-
-        monkeypatch.setenv('TRAVIS', 'true')
-        monkeypatch.setenv('TRAVIS_PYTHON_VERSION', '2.7')
-        monkeypatch.setenv('__TOX_TRAVIS_SYS_VERSION', 'CPython,2,7')
-
+        tox_ini = tox_ini_factors_override
+        self.configure(tmpdir, monkeypatch, tox_ini, 'CPython', 2, 7)
         assert self.tox_envs() == ['py27-django']
 
     def test_factors(self, tmpdir, monkeypatch):
-        os.chdir(str(tmpdir))
-        tmpdir.join('tox.ini').write(tox_ini_factors)
-
-        monkeypatch.setenv('TRAVIS', 'true')
-        monkeypatch.setenv('TRAVIS_PYTHON_VERSION', '3.4')
-        monkeypatch.setenv('__TOX_TRAVIS_SYS_VERSION', 'CPython,3,4')
-
+        tox_ini = tox_ini_factors
+        self.configure(tmpdir, monkeypatch, tox_ini, 'CPython', 3, 4)
         assert self.tox_envs() == ['py34', 'py34-docs', 'py34-django']
 
     def test_match_and_keep(self, tmpdir, monkeypatch):
-        os.chdir(str(tmpdir))
-        tmpdir.join('tox.ini').write(tox_ini_factors_override_nonenvlist)
-
-        monkeypatch.setenv('TRAVIS', 'true')
-        monkeypatch.setenv('TRAVIS_PYTHON_VERSION', '3.4')
-        monkeypatch.setenv('__TOX_TRAVIS_SYS_VERSION', 'CPython,3,4')
-
+        tox_ini = tox_ini_factors_override_nonenvlist
+        self.configure(tmpdir, monkeypatch, tox_ini, 'CPython', 3, 4)
         assert self.tox_envs() == ['py34', 'py34-docs', 'py34-django',
                                    'extra-coveralls', 'extra-flake8']
 
     def test_django_factors(self, tmpdir, monkeypatch):
-        os.chdir(str(tmpdir))
-        tmpdir.join('tox.ini').write(tox_ini_django_factors)
-
-        monkeypatch.setenv('TRAVIS', 'true')
-        monkeypatch.setenv('TRAVIS_PYTHON_VERSION', '2.7')
-        monkeypatch.setenv('__TOX_TRAVIS_SYS_VERSION', 'CPython,2,7')
-
+        tox_ini = tox_ini_django_factors
+        self.configure(tmpdir, monkeypatch, tox_ini, 'CPython', 2, 7)
         assert self.tox_envs() == ['py27-django', 'py34-django']
 
     def test_non_python_factor(self, tmpdir, monkeypatch):
-        os.chdir(str(tmpdir))
-        tmpdir.join('tox.ini').write(tox_ini_django_factors)
-
-        monkeypatch.setenv('TRAVIS', 'true')
-        monkeypatch.setenv('TRAVIS_PYTHON_VERSION', '3.4')
-        monkeypatch.setenv('__TOX_TRAVIS_SYS_VERSION', 'CPython,3,4')
-
+        tox_ini = tox_ini_django_factors
+        self.configure(tmpdir, monkeypatch, tox_ini, 'CPython', 3, 4)
         assert self.tox_envs() == ['other']
