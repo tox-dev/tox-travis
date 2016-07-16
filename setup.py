ac0b11f3ca3906f80564207b0d43be485b5e5617
John Vandenberg <jayvdb@gmail.com>
2016-07-16 08:34:10 +0700

    Support PyPy3 v5.2 with setuptools hackery
    PyPy has started releasing alpha releases of PyPy3 v5.2,
    and Travis now supports `python: pypy3.3-5.2-alpha1`.
    
    PyPy3 v5.2 depends on virtualenv>=15.0.2, whereas Travis installs 12.0.6.
    Only force the use of virtualenv>=15.0.2 on PyPy3 v5.2, so that existing
    users of tox-travis are not impacted.
    This is complicated by Travis providing setuptools 12.0.5,
    which does not support `platform_python_implementation=="PyPy"`
    as an environment marker.  Prior to setuptools 20.10.0 there is patchy
    support for environment markers, and setup.py fails while parsing them.
    
    To avoid breakage if someone is installing tox-travis using an older
    setuptools, setup.py patches _markerlib and pkg_resources so they perform
    correct environment marker matching.
    
    However the typical Travis-CI usage is via `pip install tox-travis`.
    tox-travis provides a universal wheel, which supports environment markers,
    Travis pip 6.0.7 can install this universal wheel without using setuptools.
    
    Also, fiddle with tox internals so that it attempts to
    run `python` for the Travis versions `pypy3.3-5.2-*`.

--- setup.py
+++ setup.py
@@ -1,4 +1,56 @@
-from setuptools import setup
+import sys
+
+from setuptools import setup, __version__ as setuptools_version
+from pkg_resources import parse_version
+
+import pkg_resources
+
+try:
+    import _markerlib.markers
+except ImportError:
+    _markerlib = None
+
+
+# _markerlib.default_environment() obtains its data from _VARS
+# and wraps it in another dict, but _markerlib_evaluate writes
+# to the dict while it is iterating the keys, causing an error
+# on Python 3 only.
+# Replace _markerlib.default_environment to return a custom dict
+# that has all the necessary markers, and ignores any writes.
+
+class Python3MarkerDict(dict):
+
+    def __setitem__(self, key, value):
+        pass
+
+    def pop(self, i=-1):
+        return self[i]
+
+
+if _markerlib and sys.version_info[0] == 3:
+    env = _markerlib.markers._VARS
+    for key in list(env.keys()):
+        new_key = key.replace('.', '_')
+        if new_key != key:
+            env[new_key] = env[key]
+
+    _markerlib.markers._VARS = Python3MarkerDict(env)
+
+    def default_environment():
+        return _markerlib.markers._VARS
+
+    _markerlib.default_environment = default_environment
+
+# Avoid the very buggy pkg_resources.parser, which doesnt consistently
+# recognise the markers needed by this setup.py
+# Change this to setuptools 20.10.0 to support all markers.
+if pkg_resources:
+    if parse_version(setuptools_version) < parse_version('20.10.0'):
+        MarkerEvaluation = pkg_resources.MarkerEvaluation
+
+        del pkg_resources.parser
+        pkg_resources.evaluate_marker = MarkerEvaluation._markerlib_evaluate
+        MarkerEvaluation.evaluate_marker = MarkerEvaluation._markerlib_evaluate
 
 
 def fread(fn):
@@ -21,6 +73,7 @@ setup(
     install_requires=['tox>=2.0'],
     extras_require={
         ':python_version=="3.2"': ['virtualenv<14'],
+        ':platform_python_implementation=="PyPy" and python_version=="3.3"': ['virtualenv>=15.0.2'],
     },
     classifiers=[
         'Programming Language :: Python',
