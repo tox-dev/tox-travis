44a35c021f6186ff79bac23ec85749fe5731096e
John Vandenberg <jayvdb@gmail.com>
2016-07-27 04:08:09 +0700

    Support PyPy3 v5.2 with setuptools hackery (#24)
    PyPy has started releasing alpha releases of PyPy3 v5.2,
    and Travis now supports `python: pypy3.3-5.2-alpha1`.
    
    PyPy3 v5.2 depends on virtualenv>=15.0.2, whereas Travis installs 12.0.6.
    Only force the use of virtualenv>=15.0.2 on PyPy3 v5.2, so that existing
    users of tox-travis are not impacted.
    This is complicated by Travis providing setuptools 12.0.5,
    which does not support `platform_python_implementation=="PyPy"`
    as an environment marker.  Prior to setuptools 20.10.0 there is patchy
    support for environment markers, and setup.py fails while parsing them.
    
    To avoid breakage if someone is installing tox-travis using an older
    setuptools, setup.py patches _markerlib and pkg_resources so they perform
    correct environment marker matching.
    
    However the typical Travis-CI usage is via `pip install tox-travis`.
    tox-travis provides a universal wheel, which supports environment markers,
    Travis pip 6.0.7 can install this universal wheel without using setuptools.
    
    Also, fiddle with tox internals so that it attempts to
    run `python` for the Travis versions `pypy3.3-5.2-*`.

--- src/tox_travis.py
+++ src/tox_travis.py
@@ -5,6 +5,10 @@ import py
 import tox
 
 from tox.config import _split_env as split_env
+try:
+    from tox.config import default_factors
+except ImportError:
+    default_factors = None
 
 
 @tox.hookimpl
@@ -23,6 +27,14 @@ def tox_addoption(parser):
 
     matched = match_envs(declared_envs, desired_envs)
     os.environ.setdefault('TOXENV', ','.join(matched))
+
+    # Travis virtualenv do not provide `pypy3`, which tox tries to execute.
+    # This doesnt affect Travis python version `pypy3`, as the pyenv pypy3
+    # is in the PATH.
+    # https://github.com/travis-ci/travis-ci/issues/6304
+    # Force use of the virtualenv `python`.
+    if version and default_factors and version.startswith('pypy3.3-5.2-'):
+        default_factors['pypy3'] = 'python'
 
 
 def get_declared_envs(config):
