34b14f40c1d4b0c25caf6cd5f644d9874d10905d
Ryan P Kilby <rpkilby@ncsu.edu>
2017-03-02 16:11:51 -0500

    Remove config parsing, set toxenvs in configure

--- src/tox_travis/utils.py
+++ src/tox_travis/utils.py
@@ -1,7 +1,4 @@
 """Shared constants and utility functions."""
-import sys
-import os
-import py
 
 # Mapping Travis factors to the associated env variables
 TRAVIS_FACTORS = {
@@ -35,43 +32,7 @@ def parse_dict(value):
     return dict((k.strip(), v.strip()) for k, v in pairs)
 
 
-def parser_cfg(parser):
-    """Get an IniConfig from the parser.
-
-    Use the same method to parse the arguments that Tox does.
-    Needs to have all of the options added, or else the
-    parsing may not work in some cases. Even when adding
-    ``trylast=True`` to the hook implementation, that may
-    still happen if other used plugins run first.
-
-    Figure out the configfile that Tox is going to use, and
-    create a ``py.iniconfig.IniConfig`` from that file, or
-    ``None`` if no configuration file could be found.
-    """
-    try:
-        option = parser._parse_args(sys.argv[1:])
-    except:
-        option = None
-
-    # File finding algorithm copied from Tox
-    # Except for the default basename.
-    basename = option.configfile if option else 'tox.ini'
-    if os.path.isabs(basename):
-        inipath = py.path.local(basename)
-    else:
-        for path in py.path.local().parts(reverse=True):
-            inipath = path.join(basename)
-            if inipath.check():
-                break
-        else:
-            inipath = py.path.local().join('setup.cfg')
-            if not inipath.check():
-                return  # We couldn't find a config
-
-    return py.iniconfig.IniConfig(inipath)
-
-
-def config_cfg(config):
+def get_iniconfig(config):
     """Get the IniConfig from the Tox config.
 
     The Tox config is it's own wrapper around ``IniConfig`` from
