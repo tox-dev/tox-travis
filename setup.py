3637be969c9c0c21e085ab52388e13100f8620f3
Ryan Hiebert <ryan@ryanhiebert.com>
2017-11-12 00:24:17 -0600

    Release 0.9 (#89)

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
     author_email='ryan@ryanhiebert.com',
     url='https://github.com/tox-dev/tox-travis',
     license='MIT',
-    version='0.8',
+    version='0.9',
     package_dir={'': 'src'},
     packages=find_packages('src'),
     entry_points={
