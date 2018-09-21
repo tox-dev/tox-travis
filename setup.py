80f45c03b94a0e27ef442d8ca1d190b7f1042ee7
Ryan Hiebert <ryan@ryanhiebert.com>
2018-09-21 12:07:22 -0500

    Release 0.11 (#116)
    * Drop support for Python 3.2 and 3.3 (#113).
    * Fix autogen_configs for tox 3.4.0 (#115).
    * Various documentation fixes.

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
     author_email='ryan@ryanhiebert.com',
     url='https://github.com/tox-dev/tox-travis',
     license='MIT',
-    version='0.10',
+    version='0.11',
     package_dir={'': 'src'},
     packages=find_packages('src'),
     entry_points={
