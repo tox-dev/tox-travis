553651f6518b76edaeeecb8ab19d8b5a45215885
Rui Chen <rchen@meetup.com>
2019-06-04 00:19:50 -0400

    Drop py26 and update py34 to py37

--- tests/test_envlist.py
+++ tests/test_envlist.py
@@ -20,7 +20,7 @@ source =
 
 tox_ini = b"""
 [tox]
-envlist = py26, py27, py34, pypy, pypy3, docs
+envlist = py27, py34, pypy, pypy3, docs
 """
 
 tox_ini_override = tox_ini + b"""
@@ -203,7 +203,7 @@ class TestToxEnv:
     def test_not_travis(self, tmpdir, monkeypatch):
         """Test the results if it's not on a Travis worker."""
         with self.configure(tmpdir, monkeypatch, tox_ini):
-            expected = ['py26', 'py27', 'py34', 'pypy', 'pypy3', 'docs']
+            expected = ['py27', 'py37', 'pypy', 'pypy3', 'docs']
             assert self.tox_envs() == expected
 
     def test_travis_config_filename(self, tmpdir, monkeypatch):
@@ -212,19 +212,14 @@ class TestToxEnv:
                             ini_filename='spam.ini'):
             assert self.tox_envs(ini_filename='spam.ini') == ['py36']
 
-    def test_travis_default_26(self, tmpdir, monkeypatch):
-        """Give the correct env for CPython 2.6."""
-        with self.configure(tmpdir, monkeypatch, tox_ini, 'CPython', 2, 6):
-            assert self.tox_envs() == ['py26']
-
     def test_travis_default_27(self, tmpdir, monkeypatch):
         """Give the correct env for CPython 2.7."""
         with self.configure(tmpdir, monkeypatch, tox_ini, 'CPython', 2, 7):
             assert self.tox_envs() == ['py27']
 
-    def test_travis_default_34(self, tmpdir, monkeypatch):
+    def test_travis_default_37(self, tmpdir, monkeypatch):
         """Give the correct env for CPython 3.4."""
-        with self.configure(tmpdir, monkeypatch, tox_ini, 'CPython', 3, 4):
+        with self.configure(tmpdir, monkeypatch, tox_ini, 'CPython', 3, 7):
             assert self.tox_envs() == ['py34']
 
     def test_travis_default_pypy(self, tmpdir, monkeypatch):
